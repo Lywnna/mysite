ea4d198bd9393b9a3a977b876c8e34e07bfb4ced
Lywnna <vitor.valdemarca@gmail.com>
2024-06-19 21:14:52 -0300

    Worst calculator of my life

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,39 @@ fn index() -> &'static str {
     return "Hello, world!";
 }
 
-#[get("/help/<page>")]
-fn help(page: &str) -> &str {
-    return page;
+#[get("/sum/<num1>/<num2>")]
+fn sum(num1: i128, num2: i128) -> String {
+    return format!("result: {}", (num1 + num2));
+}
+
+#[get("/minus/<num1>/<num2>")]
+fn minus(num1: i128, num2: i128) -> String {
+    return format!("result: {}", (num1 - num2));
+}
+
+#[get("/multi/<num1>/<num2>")]
+fn multi(num1: i128, num2: i128) -> String {
+    return format!("result: {}", (num1 * num2));
+}
+
+#[get("/div/<num1>/<num2>")]
+fn div(num1: i128, num2: i128) -> String {
+    return format!("result: {}", (num1 / num2));
+}
+
+#[get("/pow/<num1>")]
+fn pow(num1: i128) -> String {
+    return format!("result: {}", (num1 * num1));
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/contact", routes![help])
+        .mount("/", routes![sum])
+        .mount("/", routes![minus])
+        .mount("/", routes![multi])
+        .mount("/", routes![div])
+        .mount("/", routes![pow])
 }
 
